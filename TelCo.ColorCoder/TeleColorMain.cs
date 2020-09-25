aa57690f0ebe6852cf6477317950a9311969d3d3
venkatesh-cs <69618895+venkatesh-cs@users.noreply.github.com>
2020-09-25 20:14:34 +0530

    Removed console.Readkey from main

--- TelCo.ColorCoder/TeleColorMain.cs
+++ TelCo.ColorCoder/TeleColorMain.cs
@@ -15,7 +15,6 @@ namespace TelCo.ColorCoder
         {
             Tester.TestColor();
             ManualWriter.GenerateAndWriteManualToFile();
-            Console.ReadKey();
         }
     }
 }
